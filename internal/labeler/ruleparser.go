31f7e8fd9ef4b8f275f5cff0de63b24595bc2937
tanmancan <contact@tkarimdesign.com>
2020-08-02 00:07:53 -0400

    Added rule check for head branch names

--- internal/labeler/ruleparser.go
+++ internal/labeler/ruleparser.go
@@ -2,8 +2,10 @@ package labeler
 
 import (
 	"fmt"
+	"label-it/internal/common"
 	"label-it/internal/config"
 	"label-it/internal/gitapi"
+	"regexp"
 )
 
 // Rule rule from YAML config
@@ -17,8 +19,31 @@ type Rule struct {
 // LabelRules set of rules created from YAML config
 type LabelRules []Rule
 
+// Checks if beginning or end of a string contains an asterisk(*) rune
+func isWildCard(s string) bool {
+	if s[len(s)-1] == 42 {
+		return true
+	}
+	if s[0] == 42 {
+		return true
+	}
+	return false
+}
+
 // MatchHeadRules determines if provided pull request head branch matche the HeadRule
 func (r Rule) MatchHeadRules(pr gitapi.PullRequest) bool {
+	wildcardRule := isWildCard(r.HeadRules)
+
+	if wildcardRule == true {
+		match, err := regexp.Match(r.HeadRules, []byte(pr.Head.Ref))
+		common.CheckErr(err)
+		return match
+	}
+
+	if r.HeadRules == pr.Head.Ref {
+		return true
+	}
+
 	return false
 }
 
@@ -40,6 +65,14 @@ func RuleParser(prList gitapi.ListPullsResponse) {
 	for _, rule := range config.YamlConfig.Rules {
 		newRule := Rule{rule.Label, rule.Head, rule.Base, rule.Title}
 		labelRules = append(labelRules, newRule)
+	}
+
+	for _, pr := range prList {
+		fmt.Println(pr)
+		for _, r := range labelRules {
+			matchHead := r.MatchHeadRules(pr)
+			fmt.Println(matchHead)
+		}
 	}
 
 	fmt.Println(labelRules)
