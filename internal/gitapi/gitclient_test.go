bb93c7f6930872093a44849bca95a9380b7f6839
tanmancan <contact@tkarimdesign.com>
2020-08-10 21:40:02 -0400

    unit tests for gitapi methods

---
+++ internal/gitapi/gitclient_test.go
@@ -0,0 +1,119 @@
+package gitapi
+
+import (
+	"encoding/base64"
+	"fmt"
+	"testing"
+
+	"github.com/tanmancan/label-it/v1/internal/config"
+)
+
+func Test_buildEndpoint(t *testing.T) {
+	*&config.YamlConfig.Owner = "world"
+	*&config.YamlConfig.Repo = "Robot"
+	type args struct {
+		endpointTemplate string
+		args             []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"replace strings and digits",
+			args{
+				"Hello %[1]s, my name is %[2]s #%[3]d. Why did the %[4]s cross the network?",
+				[]interface{}{
+					42,
+					"packet",
+				},
+			},
+			"Hello world, my name is Robot #42. Why did the packet cross the network?",
+		},
+		{
+			"creates add labels endpoint",
+			args{
+				githubConfig.Endpoints.AddLabels,
+				[]interface{}{
+					44,
+				},
+			},
+			"/repos/world/Robot/issues/44/labels",
+		},
+		{
+			"creates list pulls endpoint",
+			args{
+				githubConfig.Endpoints.ListPulls,
+				[]interface{}{},
+			},
+			"/repos/world/Robot/pulls",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildEndpoint(tt.args.endpointTemplate, tt.args.args...); got != tt.want {
+				t.Errorf("buildEndpoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildAPIURL(t *testing.T) {
+	type args struct {
+		endpoint string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"build full add label url",
+			args{
+				"/repos/Hello/World/issues/55/labels",
+			},
+			"https://api.github.com/repos/Hello/World/issues/55/labels",
+		},
+		{
+			"build fake api url",
+			args{
+				"/abc/def/hij",
+			},
+			"https://api.github.com/abc/def/hij",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildAPIURL(tt.args.endpoint); got != tt.want {
+				t.Errorf("buildAPIURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildBasicAuth(t *testing.T) {
+	fakeuser := "fakeuser"
+	faketoken := "faketoken"
+	*&config.YamlConfig.Access.User = fakeuser
+	*&config.YamlConfig.Access.Token = faketoken
+	authtoken := fmt.Sprintf("%[1]s:%[2]s", fakeuser, faketoken)
+	fakebasicauth := base64.StdEncoding.EncodeToString([]byte(authtoken))
+	fakebasicauth = fmt.Sprintf("Basic %[1]s", fakebasicauth)
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			"correctly encodes username and token",
+			fakebasicauth,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildBasicAuth(); got != tt.want {
+				t.Errorf("buildBasicAuth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
