3e16431529bd22729b4a06e61b647d1b2c995568
tanmancan <contact@tkarimdesign.com>
2020-09-27 22:12:10 -0400

    removed flag check from labeler

--- internal/labeler/labelpr.go
+++ internal/labeler/labelpr.go
@@ -2,39 +2,20 @@ package labeler
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/tanmancan/label-it/v1/internal/config"
 	"github.com/tanmancan/label-it/v1/internal/gitapi"
 )
-
-func printPrLabel(prLabels []gitapi.PrLabel) {
-	fmt.Println("PR\tLabels")
-	fmt.Println("--\t------")
-	for _, prLabel := range prLabels {
-		fmt.Printf("%[1]d\t%[2]s\n", prLabel.Issue, strings.Join(prLabel.Labels, ", "))
-	}
-	fmt.Print("\n")
-}
 
 // LabelPr adds labels to a given list of pull requests via the Github API
 func LabelPr(prLabels []gitapi.PrLabel) {
 	updateCount := len(prLabels)
-	fmt.Printf("Found %[1]d matching pull request.\n", updateCount)
-	printPrLabel(prLabels)
 
-	if config.DryRun == true {
-		fmt.Println("Perform dry run. Pull requests were not updated.")
+	c := make(chan string, updateCount)
+	for _, prLabel := range prLabels {
+		go gitapi.AddLabels(prLabel, c)
 	}
 
-	if config.DryRun == false {
-		c := make(chan string, updateCount)
-		for _, prLabel := range prLabels {
-			go gitapi.AddLabels(prLabel, c)
-		}
-
-		for i := 0; i < updateCount; i++ {
-			fmt.Println(<-c)
-		}
+	for i := 0; i < updateCount; i++ {
+		fmt.Println(<-c)
 	}
 }
